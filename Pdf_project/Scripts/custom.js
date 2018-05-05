a4f97accdf463eaf5dd7f4566ea74f641306594e
Nenad <nenadnikolic24@gmail.com>
2018-05-05 18:17:37 +0200

    Added logout div, dashboard style, fixed position

--- Pdf_project/Scripts/custom.js
+++ Pdf_project/Scripts/custom.js
@@ -1,6 +1,17 @@
 ﻿
 jQuery(document).ready(function () {
+
+    //Function that triggers on login button
     jQuery(document).on('click', "#login", function () {
         
     });
+
+
+    //Function that trigger on triange bottom icon on dashboard, will toggle logout div
+    jQuery(document).on('click', '.triangle-bottom', function () {
+        jQuery('.logout-div').toggle();
+    });
+
+
+
 });
